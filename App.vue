d7c1293f815856e3497c7d21f8cd770355c84432
OlaAL <100041114+OlaAL@users.noreply.github.com>
2022-05-10 09:25:26 +0300

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,24 @@
+<script setup>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  
+  <img alt="todo logo" src="./assets/1.jpg" />
+ 
+  
+  <HelloWorld msg="Hello " />
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
